074e7d897d6528d369511bcddaf640f49fcffe94
Ifeanyi Nwosu <nwosuifeanyi9@gmail.com>
2022-12-15 12:02:59 -0800

    new

--- pall.c
+++ pall.c
@@ -1,21 +1,13 @@
 #include "monty.h"
+
 /**
- * f_pall - prints the stack
- * @head: stack head
- * @counter: no used
- * Return: no return
+ * pall - prints the stack
+ * @stack: stack given by main in start.c
+ * @line_cnt: amount of lines
+ *
+ * Return: void
  */
 void f_pall(stack_t **head, unsigned int counter)
 {
-	stack_t *h;
-	(void)counter;
-
-	h = *head;
-	if (h == NULL)
-		return;
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-	}
+	print_stack(*stack);
 }
